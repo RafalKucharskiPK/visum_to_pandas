f01a27db2411ae38ec0a2b35df769ddad954cdf7
Rafal Kucharski <rafal.kucharski@northgravity.com>
2018-03-29 16:03:27 +0200

    test files

--- ptv_visum_to_pandas.py
+++ ptv_visum_to_pandas.py
@@ -7,8 +7,8 @@ TABLE_NAME_HEADER = "* Table: "
 COL_DEF_HEADER = "$"
 
 #paths
-PATH = "/Users/rafal/Google Drive/data/MOMM_net.net"
-OUTPATH = "/Users/rafal/Google Drive/data/visum/"
+PATH = "./test/MOMM_net.net"
+OUTPATH = "./test/data/"
 IS_TABLE_FLAG = False
 
 with codecs.open(PATH, encoding='utf-8', errors='ignore') as net:
@@ -16,12 +16,12 @@ with codecs.open(PATH, encoding='utf-8', errors='ignore') as net:
     for line in net:
         if line.startswith(TABLE_NAME_HEADER):
             # new table
-            tablename = line.split(":")[-1][1:-1]
+            table_name = line.split(":")[-1][1:-1]
 
         if line[0] == COL_DEF_HEADER:
             # line with column names
             IS_TABLE_FLAG = True
-            data = list() #initialize data
+            data = list() # initialize data
             try:
                 cols = line.split(":")[1].split(COL_DELIMITER)
             except:
@@ -30,7 +30,7 @@ with codecs.open(PATH, encoding='utf-8', errors='ignore') as net:
             IS_TABLE_FLAG = False
             if len(data) > 0:
                 # only if table is not empty
-                pd.DataFrame(data, columns=cols).to_csv(OUTPATH+tablename+".csv")
+                pd.DataFrame(data, columns=cols).to_csv(OUTPATH+table_name+".csv")
         if IS_TABLE_FLAG and line[0] != COL_DEF_HEADER:
             data.append(line.split(COL_DELIMITER))
 
