48cc48c374eb90ad14bd1169bf899ea4cccf0f98
Rafal Kucharski <rafal.kucharski@northgravity.com>
2018-03-29 16:06:19 +0200

    test files

--- ptv_visum_to_pandas.py
+++ ptv_visum_to_pandas.py
@@ -7,32 +7,39 @@ TABLE_NAME_HEADER = "* Table: "
 COL_DEF_HEADER = "$"
 
 #paths
-PATH = "./test/MOMM_net.net"
+NETPATH = "./test/MOMM_net.net"
 OUTPATH = "./test/data/"
-IS_TABLE_FLAG = False
 
-with codecs.open(PATH, encoding='utf-8', errors='ignore') as net:
+def parse_net():
 
-    for line in net:
-        if line.startswith(TABLE_NAME_HEADER):
-            # new table
-            table_name = line.split(":")[-1][1:-1]
+    _table_flag = False
 
-        if line[0] == COL_DEF_HEADER:
-            # line with column names
-            IS_TABLE_FLAG = True
-            data = list() # initialize data
-            try:
-                cols = line.split(":")[1].split(COL_DELIMITER)
-            except:
-                cols = line.split(COL_DELIMITER)
-        if line == '\n':
-            IS_TABLE_FLAG = False
-            if len(data) > 0:
-                # only if table is not empty
-                pd.DataFrame(data, columns=cols).to_csv(OUTPATH+table_name+".csv")
-        if IS_TABLE_FLAG and line[0] != COL_DEF_HEADER:
-            data.append(line.split(COL_DELIMITER))
+    with codecs.open(NETPATH, encoding='utf-8', errors='ignore') as net:
+
+        for line in net:
+            if line.startswith(TABLE_NAME_HEADER):
+                # new table
+                table_name = line.split(":")[-1][1:-1]
+
+            if line[0] == COL_DEF_HEADER:
+                # line with column names
+                _table_flag = True
+                data = list()  # initialize data
+                try:
+                    cols = line.split(":")[1].split(COL_DELIMITER)
+                except:
+                    cols = line.split(COL_DELIMITER)
+            if line == '\n':
+                _table_flag = False
+                if len(data) > 0:
+                    # only if table is not empty
+                    pd.DataFrame(data, columns=cols).to_csv(OUTPATH+table_name+".csv")
+            if _table_flag and line[0] != COL_DEF_HEADER:
+                data.append(line.split(COL_DELIMITER))
+
+
+if __name__ == "__main__":
+    parse_net()
 
 
 
