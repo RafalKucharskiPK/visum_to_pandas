71443496e1a88dc98f44fb5aa1916336a4856dbf
Rafal Kucharski <rkucharski@pk.edu.pl>
2018-03-29 15:59:47 +0200

    Add files via upload

---
+++ ptv_visum_to_pandas.py
@@ -0,0 +1,43 @@
+import codecs
+import pandas as pd
+
+# semantics
+COL_DELIMITER = ";"
+TABLE_NAME_HEADER = "* Table: "
+COL_DEF_HEADER = "$"
+
+#paths
+PATH = "/Users/rafal/Google Drive/data/MOMM_net.net"
+OUTPATH = "/Users/rafal/Google Drive/data/visum/"
+IS_TABLE_FLAG = False
+
+with codecs.open(PATH, encoding='utf-8', errors='ignore') as net:
+
+    for line in net:
+        if line.startswith(TABLE_NAME_HEADER):
+            # new table
+            tablename = line.split(":")[-1][1:-1]
+
+        if line[0] == COL_DEF_HEADER:
+            # line with column names
+            IS_TABLE_FLAG = True
+            data = list() #initialize data
+            try:
+                cols = line.split(":")[1].split(COL_DELIMITER)
+            except:
+                cols = line.split(COL_DELIMITER)
+        if line == '\n':
+            IS_TABLE_FLAG = False
+            if len(data) > 0:
+                # only if table is not empty
+                pd.DataFrame(data, columns=cols).to_csv(OUTPATH+tablename+".csv")
+        if IS_TABLE_FLAG and line[0] != COL_DEF_HEADER:
+            data.append(line.split(COL_DELIMITER))
+
+
+
+
+
+
+
+
