1c01e88b3c75e433429e0e487cdfaa9eb6780968
Rafal Kucharski <rkucharski@pk.edu.pl>
2018-07-16 13:21:15 +0200

    Skrypt do pracy przy grancie Polonez

--- Polonez.py
+++ Polonez.py
@@ -48,6 +48,7 @@ Length
 Time 
 """
 def SPS_PrT(_from, _to):
+    # Szuka sciezki w sieci drogowej dla zadanego kryterium pomiedzy zadana para punktow, zwraca zadany atrybut
     RouteSearch = Visum.Analysis.RouteSearchPrT
     Route = Visum.CreateNetElements()
     RouteSearch.Clear()
@@ -57,13 +58,13 @@ def SPS_PrT(_from, _to):
     return RouteSearch.AttValue(ATT)
 
 def SPS_PuT(_from, _to):
+    # Szuka sciezki w sieci KZ dla zadanego kryterium pomiedzy zadana para punktow, zwraca zadany atrybut
     RouteSearch = Visum.Analysis.RouteSearchPuT
     Route = Visum.CreateNetElements()
     RouteSearch.Clear()
     Route.Add(_from)
     Route.Add(_to)
     RouteSearch.Execute(Route,MODE, DEP_TIME)
-
     return RouteSearch.AttValue(ATT_PUT)
 
 def POI2NearestNode(Visum):
@@ -95,40 +96,45 @@ def POI2NearestSPoint(Visum):
 
 
 def MainLoop(Visum):
+    Visum.Graphic.StopDrawing = True  # nie rysuj (przyspieszenie)
+    # inicjalizacja bazy dancyh (do csv)
     df = pd.DataFrame(columns=["Z_Rejon", "Do_Rejon", "L_Podrozy", "Przez_POI", "Czas_PrT", "Czas_PuT"])
 
-    Zones = Visum.Net.Zones.GetMultiAttValues("No")
+    Zones = Visum.Net.Zones.GetMultiAttValues("No")  # rejony do iteracji
+    # dane o POI
     POIs = Visum.Net.POICategories.ItemByKey(1).POIs.GetMultipleAttributes(["No", "Node", "SPoint", "Dist_PrT", "Dist_PuT"])
 
+    # glowna petla
     for OZone in Zones:
         for DZone in Zones:
             if OZone != DZone:
-                nTrips = Visum.Net.Matrices.ItemByKey(MATRIX_NO).GetValue(OZone[1],DZone[1])
+                nTrips = Visum.Net.Matrices.ItemByKey(MATRIX_NO).GetValue(OZone[1],DZone[1]) # liczba podrozy (z macierzy)
+                Z = Visum.Net.Zones.ItemByKey(OZone[1]) # para rejonow Z
+                Do = Visum.Net.Zones.ItemByKey(DZone[1]) # i do
                 for POI in POIs:
-                    Z = Visum.Net.Zones.ItemByKey(OZone[1])
-                    Przez_PrT = Visum.Net.Nodes.ItemByKey(POI[1])
-                    Do = Visum.Net.Zones.ItemByKey(DZone[1])
-                    CzPrT = SPS_PrT(Z, Przez_PrT) + SPS_PrT(Przez_PrT, Do) + 2 * POI[3]
+                    Przez_PrT = Visum.Net.Nodes.ItemByKey(POI[1]) #Punkt w sieci dla POI
+                    CzPrT = SPS_PrT(Z, Przez_PrT) + SPS_PrT(Przez_PrT, Do) + 2 * POI[3] # Oblicz czas PrT (2x dojscie do POI)
 
                     if POI[2] is not None:
-                        Przez_PuT = Visum.Net.StopAreas.ItemByKey(POI[2])
-                        CzPuT = SPS_PuT(Z, Przez_PuT) + SPS_PuT(Przez_PuT, Do)+ 2*POI[4]
+                        Przez_PuT = Visum.Net.StopAreas.ItemByKey(POI[2]) # Przystanek dla POI (jesli jest)
+                        CzPuT = SPS_PuT(Z, Przez_PuT) + SPS_PuT(Przez_PuT, Do)+ 2*POI[4]   # Oblicz czas PuT (2x dojscie do POI)
                     else:
                         CzPuT = 999999
                     print("From {} to {} Via {} in {} PrT, {} PuT".format(OZone[1], DZone[1], int(POI[0]), CzPrT, CzPuT))
-                    df.loc[df.shape[0] + 1] = [OZone[1], DZone[1], nTrips, int(POI[0]), CzPrT, CzPuT]
-    df.to_csv("POIs.csv")
+                    df.loc[df.shape[0] + 1] = [OZone[1], DZone[1], nTrips, int(POI[0]), CzPrT, CzPuT] # zapisz rekord w bazie danych
+    df.to_csv("POIs.csv")  # zapisz baze do pliku
+    Visum.Graphic.StopDrawing = False
 
 
 if __name__ == "__main__":
 
-    Visum = win32com.client.Dispatch("Visum.Visum")
-    Visum.LoadVersion(VISUM_PATH)
+    Visum = win32com.client.Dispatch("Visum.Visum")  # uruchom Visum
+    Visum.LoadVersion(VISUM_PATH)  # zaladuj plik
 
-    POI2NearestNode(Visum)
-    POI2NearestSPoint(Visum)
+    POI2NearestNode(Visum) # przypisz wezly sieci do POI
+    POI2NearestSPoint(Visum) # przypisz przystanki do POI
 
-    MainLoop(Visum)
+    MainLoop(Visum) # glowny algorytm
     #Visum.SaveVersion(VISUM_PATH)
 
 
